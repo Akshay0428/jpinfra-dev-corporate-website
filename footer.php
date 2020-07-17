45abbc39569f7bd3ae5a620487db631099b149b9
Shruti0398 <shrusp98@gmail.com>
2020-07-17 13:24:25 +0530

    disclemers added and image of overview changed

--- footer.php
+++ footer.php
@@ -40,7 +40,27 @@
 <footer class="mainfooter">
 	<div class="content-continer">
 		<div class="col-md-8 col-lg-8 footer-text">
-			<p>Copyright by JP INFRA. All rights reserved.</p>
+			<div id="section-foot">
+				<div class="article">
+				    <p>Thank you for visiting JP Infra’s website.
+				    <p class="moretext-foot">
+				      This is to inform user / customer(s) that jpinfra.com is/are the only official websites of JP Infra (Mumbai) Private Limited (“Company”). User/Customer(s) are cautioned and advised not to rely upon any information stated on any other websites which may appear to be similar to the company’s official website, including containing company’s logo / brand name. The information contained in such websites may be misleading/false and user/customer(s) may suffer loss if they rely on such information. In the event user/customer(s) come across any such websites similar to company’s official website containing its brand name/logo or any other information, then kindly contact and inform us on sales@jpinfra.com in order to enable the Company to take necessary action. Please ensure that you deal with only RERA registered real estate agents (“Registered Real Estate Agents”) whose name appear as a real estate agent under the project name on the RERA web site. If you have received any unsolicited calls or text messages in breach of the regulatory norms or from agents not registered with us, please do not trust such information and report such instances to us on sales@jpinfra.com for our further investigation.
+						Specifications and amenities mentioned in this website are only representative and informative. Information, images and visuals, drawings, plans or sketches shown in this website are only an architect’s impression and are only indicative of the envisaged developments and the same are subject to approvals from local authorities and finalization by the Company. The Company reserves rights to make additions, deletions, alterations or amendments as and when it deems fit and proper, without any prior notice. No representation or warranty is made or intended as to the accuracy or completeness of information and no commitments are being given under this website as to it’s suitability or adequacy for any purpose or otherwise howsoever.
+						All dimensions mentioned in the drawings may vary/differ due to construction contingencies and site conditions. Actual product/ development and any other aspect may differ from what is portrayed herein. All layouts, plans, specifications, dimensions, designs, measurements and locations are indicative and not to scale and are subject to change as may be decided by the company and/or competent authority. Revision, alteration, modification, addition, deletion, substitution or recast, if any, may be necessary during construction. For accurate information, you are requested to contact the Company’s authorized sales personnel.
+						We endeavor to keep the information up to date and correct however we make no representations, guarantees, or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services or related graphics contained on the website for any purpose.
+						Information about projects is indicative. Artist's impressions, products, features, furniture, floor coverings, curtains, mirrors, wall hangings, light fittings, furnishings, pictures, images, plans, drawings, amenities, dimensions, elevations etc., if any, are shown as illustrations and for reference only.
+						The views shown in the picture may vary over period of time and Company does not guarantee the same. All intending purchaser/s are bound and expected to inspect all plans and approvals and visit project site and apprise themselves of all plans and approvals and other relevant information and nothing in this website is intended to substitute to the intending purchaser the actual plans and approvals obtained from time to time. The relationship between the Company and the actual customers will be governed strictly by the agreement for sale to be executed from time to time and not on the terms of this website. All details and prices quoted are indicative and have been calculated on basic rate.
+						This website is merely conceptual and is not a legal document. It cannot be treated as a part of the final purchase agreement/s that may be executed from time to time. The Company reserves right to change, amend, modify the contents of the brochure and architectural specifications during development stages. The Company/its Management and employees hold no responsibility for its accuracy and shall not be liable to any intending purchaser or any one for the changes/ alterations/ improvements so made.
+						The copyright, database rights, trademarks, patents, know how, confidential information and personal data and all other intellectual property contained in this website is the property of JP Infra and/or its affiliates.
+						The Company uses the information that we collect on the Site to contact you when requested in order to further discuss your interest in our company, our services, and to send information regarding our company or partners, such as marketing promotions and events. This information may also be used to improve the services we provide you. The information is collected and stored in a manner that is appropriate for the nature of the data that we collect, and the need to fulfill your request. This information is not provided to third parties for their use, unless specifically authorized by you.
+						The Company or its directors or employees will not be liable for any loss, damage, errors, injury, whether direct, indirect, consequential or incidental, suffered or incurred by any person/s or due to any use and/or inability to use this site or information or its links, hyperlinks, action taken or abstained, any transmission made through this site and news groups, even if the Company and/or its employees or directors have been notified of the possibilities of potential damages or losses.
+						Any recipient or user of any information or material from this site may avail of the same entirely at his/her own risk as to costs and consequences, thereof.
+						For further information on the registered projects please refer to 'https://maharera.mahaonline.gov.in/'
+
+				    </p>
+			  	</div>
+			  	<a class="moreless-button">Read more</a>
+			</div>
 		</div>
 		<div class="col-md-4 col-lg-4 footer-icons">
 			<a href="https://www.facebook.com/jpinfraofficial" target="_blank">
@@ -168,6 +188,17 @@
             
         </script>
 
+        <!-- disclimers -->
+        <script>
+        	$('.moreless-button').click(function() {
+			  $('.moretext-foot').slideToggle();
+			  if ($('.moreless-button').text() == "Read more") {
+			    $(this).text("Read less")
+			  } else {
+			    $(this).text("Read more")
+			  }
+			});
+        </script>
 
     <!-- navigation sub-menu display -->
 <!-- 		<script>
